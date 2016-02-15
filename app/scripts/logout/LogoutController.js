82673c069bfa3c9c7a5d68805abd49727efae740
leito9008 <leian1306@gmail.com>
2016-02-15 18:44:21 -0300

    Se agrega un controller para logout y otro para manejar info en el side menu

---
+++ app/scripts/logout/LogoutController.js
@@ -0,0 +1,33 @@
+/* jshint bitwise: false, camelcase: false, curly: true, eqeqeq: true, globals: false, freeze: true, immed: true, nocomma: true, newcap: true, noempty: true, nonbsp: true, nonew: true, quotmark: double, undef: true, unused: true, strict: true, latedef: nofunc */
+
+/* globals angular */
+
+/**
+ * @ngdoc function
+ * @name TracsClient.controllers:LogoutController
+ * @description
+ * Controlador que maneja el logout de un usuario
+ */
+
+(function () {
+    "use strict";
+
+    angular
+        .module("TracsClient.controllers")
+        .controller("LogoutController", LogoutController);
+
+    LogoutController.$inject = ["$state", "localStorageService"];
+
+    function LogoutController($state, localStorageService) {
+
+        activate();
+
+        function activate() {
+            // Borra los datos del localStorage
+            localStorageService.clearAll();
+            // Redirige al login
+            $state.go("app.login");
+        }
+    }
+
+})();
---
+++ app/scripts/menu/MenuController.js
@@ -0,0 +1,40 @@
+/* jshint bitwise: false, camelcase: false, curly: true, eqeqeq: true, globals: false, freeze: true, immed: true, nocomma: true, newcap: true, noempty: true, nonbsp: true, nonew: true, quotmark: double, undef: true, unused: true, strict: true, latedef: nofunc */
+
+/* globals angular */
+
+/**
+ * @ngdoc function
+ * @name TracsClient.controllers:MenuController
+ * @description
+ * Controlador que maneja la información del usuario que se muestra en el menú
+ */
+
+(function () {
+    "use strict";
+
+    angular
+        .module("TracsClient.controllers")
+        .controller("MenuController", MenuController);
+
+    MenuController.$inject = ["$scope", "$rootScope", "localStorageService"];
+
+    function MenuController($scope, $rootScope, localStorageService) {
+
+        var vm = this;
+
+        /**
+         * Suscripción al evento "user.changed", que se ejecuta cuando
+         * cambiaron los datos de usuario en la sesión
+         */
+        var userChangedEvent = $rootScope.$on("user.changed", function () {
+            // Asigna el usuario logueado
+            vm.user = localStorageService.get("user");
+        });
+
+        /**
+         * Dessucbribe el evento para evitar leaks de memoria
+         */
+        $scope.$on("$destroy", userChangedEvent);
+    }
+
+})();
