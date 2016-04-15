8a2a3f6cae3a21742820254e67d17b5f7b975a12
leito9008 <leian1306@gmail.com>
2016-04-15 00:11:06 -0300

    Se agrego la libreria moment y un filtro para mostrar las fechas de las notificaciones

---
+++ app/scripts/filters/TwitterLikeDateFilter.js
@@ -0,0 +1,23 @@
+/**
+ * @ngdoc filter
+ * @name TracsClient.filters:TwitterLikeDateFilter
+ * @external https://github.com/hijonathan/moment.twitter
+ * @description
+ * Filtro que devuelve la fecha en formato twitter, tipo: 6 days
+ */
+
+(function () {
+    "use strict";
+
+    angular
+        .module("TracsClient.filters")
+        .filter("twitterDate", TwitterLikeDateFilter);
+
+    TwitterLikeDateFilter.$inject = ["moment"];
+
+    function TwitterLikeDateFilter(moment) {
+        return function (inputDate) {
+            return moment(inputDate).twitter();
+        };
+    }
+}());
